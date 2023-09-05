ffb2d3d96c91176d9657d950c567ee19c23d74e0
Andry Kamamy <tchithierry@gmail.com>
2023-09-05 17:39:16 +0300

    Ajout des éléments du components et mise à jour du fichier inventory

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      
+    </Router>
   )
 }
 
---
+++ src/data/inventory.js
@@ -0,0 +1,40 @@
+import {va as uuidv4} from "uuid"
+
+const inventory = [
+    {
+        title: "Mug Coding Fuel",
+        price: 14.99,
+        img:"codinfuel",
+        id: uuidv4(),
+    },
+    {
+        title: "Mug CSS",
+        price: 14.99,
+        img:"cssmug",
+        id: uuidv4(),
+    },
+    {
+        title: "Mug Coding Fuel",
+        price: 14.99,
+        img:"codinfuel",
+        id: uuidv4(),
+    },
+    {
+        title: "Mug JS",
+        price: 14.99,
+        img:"jsmug",
+        id: uuidv4(),
+    },
+    {
+        title: "Mug React",
+        price: 14.99,
+        img:"reactmug",
+        id: uuidv4(),
+    },
+    {
+        title: "Mug love JS",
+        price: 14.99,
+        img:"lovejs",
+        id: uuidv4(),
+    },
+]
